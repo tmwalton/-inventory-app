351104939e036aefb9b2f8e69974118533e390bd
Tobin <twalton@kickbackpoints.com>
2015-07-23 10:06:59 -0600

    Total Landed Value column is dynamically updated, currency formatted

--- inventory.js
+++ inventory.js
@@ -29,7 +29,7 @@ if (Meteor.isClient) {
 			var raw_cost = event.target.raw_cost.value;
 			var landed_cost = event.target.landed_cost.value;
 			var qty_in_stock = event.target.qty_in_stock.value;
-			var total_landed_value_in_stock = event.target.total_landed_value_in_stock.value;
+			var total_landed_value_in_stock = qty_in_stock * landed_cost;
 			
 			Items.insert({
 				product: product,
@@ -46,7 +46,6 @@ if (Meteor.isClient) {
 			event.target.raw_cost.value = "";
 			event.target.landed_cost.value = "";
 			event.target.qty_in_stock.value = "";
-			event.target.total_landed_value_in_stock.value = "";
 		}
 	});
 	
@@ -55,18 +54,32 @@ if (Meteor.isClient) {
 			var productId = this._id;
 			Session.set('selectedProduct', productId);
 			var selectedProduct = Session.get('selectedProduct');
-			console.log(selectedProduct);
 		},
+		
 		"keydown .quantity-field": function(event) {
+			
+			var productId = this._id;
+			Session.set('selectedProduct', productId);
+			//var cost = Items.find({"_id": this._id}, {fields: {'landedCost':1, '_id':0}}).fetch();
+			var cost = Items.findOne({'_id':this._id}, {'landedCost': 1}).landedCost;
+			
 			if (event.keyCode == 13) {
 				var newQuantity = event.target.value;
-				console.log(newQuantity);
 				var self = this;
 				self.value = newQuantity;
+				
+				var newValue = "$" + cost * newQuantity + ".00"; // Move currency formatting logic to onRendered function
+				
 				Items.update(this._id, {$set:{qtyInStock: newQuantity}});
+				Items.update(this._id, {$set:{totalLandedValueInStock: newValue}});
 				console.log('updated');
 			}
 		}
+	});
+	
+	Template.table.onRendered({
+		// Create function to format table cells for currency on rendering so 
+		// as not to pollute data
 	});
 }
 
